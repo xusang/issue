643d4becb2cce16f66b340e521f490a2160c3610
sang.xu <371815423@qq.com>
2018-05-24 16:21:42 +0800

    队列demo

---
+++ queue/queue.js
@@ -0,0 +1,55 @@
+const isSellerQueue = (callback) => {
+    // 判断标志位 是否存在身份标志 默认-1
+    if (window.is_seller_type === 1 || window.is_seller_type === 0) { // 存在标志位判断身份，直接回调
+        callback(window.is_seller_type)
+    } else { // 不存在标志
+        // 判断标志位 是否hybrid执行中
+        if (window.is_seller_is_queue) {
+            // 如果不存在任务数组队列则创建数组
+            if (!window.is_seller_queue_arr) {
+                window.is_seller_queue_arr = [];
+            }
+            window.is_seller_queue_arr.push(callback);
+        } else {
+            // hybrid不在执行中
+            window.is_seller_is_queue = 1;
+            window.window.hybrid && window.hybrid.config({
+                jsApiList: {getUserInfo: 'optional'}
+            }, function (error, result) {
+                if (error) {
+                    window.is_seller_is_queue = 0;
+                    return;
+                }
+                // result中无type
+                if (result[0] !== 'getUserInfo') {
+                    window.is_seller_type = 0;
+                    window.is_seller_is_queue = 0;
+                    return;
+                }
+                window.hybrid.getUserInfo(function (error, res) {
+                    if (error) {
+                        window.is_seller_is_queue = 0;
+                        return;
+                    }
+                    if (res.type * 1 === 2) {
+                        window.is_seller_type = 1;
+                    } else {
+                        window.is_seller_type = 0;
+                    }
+                    callback(window.is_seller_type);
+                    // 若存在未执行队列
+                    if (window.is_seller_queue_arr && window.is_seller_queue_arr !== 0) {
+                        window.is_seller_queue_arr.forEach(function(ele, index){
+                            typeof ele === 'function' && ele(window.is_seller_type);
+                            window.is_seller_queue_arr[index] = null;
+                        })
+                    }
+                    // 去除执行中
+                    window.is_seller_is_queue = 0;
+                });
+            });
+        }
+    }
+}
+
+export default isSellerQueue
